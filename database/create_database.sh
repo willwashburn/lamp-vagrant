3f307067376aa804c2bff62c3d92cd62bb1b6a7e
Will Washburn <will.washburn@gmail.com>
2013-05-07 12:21:38 -0400

    allow remote access to db

--- database/create_database.sh
+++ database/create_database.sh
@@ -4,8 +4,9 @@ PASSWORDB='abcd1234';
 
 Q1="CREATE DATABASE IF NOT EXISTS datastore;"
 Q2="GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root';"
+Q4="GRANT ALL ON *.* TO 'root'@'10.0.2.15' IDENTIFIED BY 'root';"
 Q3="FLUSH PRIVILEGES;"
-SQL="${Q1}${Q2}${Q3}"
+SQL="${Q1}${Q2}${Q4}${Q3}"
  
 mysql -uroot -p${PASSWORDB} -e "$SQL"
 mysql -uroot -p${PASSWORD} datastore < /home/vagrant/database/database_schema.sql;
